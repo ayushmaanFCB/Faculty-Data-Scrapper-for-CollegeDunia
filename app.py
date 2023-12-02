aa662b04f302c74fa1fe9e38f21200a5c53d335e
Ayushmaan Das <dasayush5maan@gmail.com>
2023-12-03 02:10:17 +0530

    location input scrapping

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from scrapper import scrap_college_dunia
-from college_links_generator import generate_statewise_links
 import pandas as pd
 
-url = "https://collegedunia.com/btech/tripura-colleges"
+try:
+    from scrapper import scrap_college_dunia
+    from college_links_generator import generate_statewise_links
+    print("\033[1;32;40mDrivers installed and Loaded successfully !!!\033[m")
+
+except:
+    print(
+        "\n\n\033[1;31;40mSome Error occurred, check Internet Connection and make sure you have Chrome !!!\033[m")
+
+location = str(input(
+    "\n\n\033[1;33;40mENTER THE STATE NAME AND CITY NAME (please enter accurate spelling)  :  \033[m"))
+url = f"https://collegedunia.com/btech/{location.lower()}-colleges"
 final_data = []
 
-links = generate_statewise_links(url)
+try:
+    links = generate_statewise_links(url)
 
-for link in links:
-    scrapped_data = scrap_college_dunia(link)
-    print(scrapped_data)
-    for d in scrapped_data:
-        final_data.append(d)
+    for link in links:
+        scrapped_data = scrap_college_dunia(link)
+        print(scrapped_data)
+        for d in scrapped_data:
+            final_data.append(d)
 
-data_df = pd.DataFrame(final_data)
-data_df.to_excel("./data/tripura.xlsx")
+    data_df = pd.DataFrame(final_data)
+    data_df.to_excel(f"./data/{location}.xlsx")
+    print(
+        f"\n\n\033[1;32;40mFaculty Details from {location} saved successfully !!!\033[m")
+
+except:
+    print(
+        "\n\n\033[1;31;40mSome Error occurred, check the input properly !!!\033[m")
