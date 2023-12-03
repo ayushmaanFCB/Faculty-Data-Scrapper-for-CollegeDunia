280b2dbd8d60491e365d723c4f0a32be753aeb53
Ayushmaan Das <dasayush5maan@gmail.com>
2023-12-04 01:21:16 +0530

    automated-page-loading

---
+++ automated_scrapping.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+try:
+    from scrapper import scrap_college_dunia
+    from college_links_generator import generate_statewise_links
+    print("\033[1;32;40m\n\nDrivers installed and Loaded successfully !!!\033[m")
+
+except:
+    print(
+        "\n\n\033[1;31;40mSome Error occurred, check Internet Connection and make sure you have Chrome !!!\033[m")
+
+for location in ("maharashtra", "andhra-pradesh", "karnataka", "kerala", "west-bengal", "telangana", "delhi-ncr"):
+    final_data = []
+    url = f"https://collegedunia.com/btech/{location.lower()}-colleges"
+
+    try:
+        links = generate_statewise_links(url)
+
+        for link in links:
+            print(link)
+            scrapped_data = scrap_college_dunia(link)
+            print(scrapped_data)
+            for d in scrapped_data:
+                final_data.append(d)
+
+        data_df = pd.DataFrame(final_data)
+        data_df.to_excel(f"./data/{location}.xlsx")
+        print(
+            f"\n\n\033[1;32;40mFaculty Details from {location} saved successfully !!!\033[m")
+
+    except:
+        print(
+            f"\n\n\033[1;31;40mSome Error occurred, check the input for {location.lower()} properly !!!\033[m")
--- college_links_generator.py
+++ college_links_generator.py
@@ -1,14 +1,40 @@
-from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup as bs
+
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 def generate_statewise_links(url):
     driver.get(url)
-    content = bs(driver.page_source, 'html.parser')
+
+    def scroll_to_bottom():
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        driver.implicitly_wait(2)
+
+    while True:
+        before_height = driver.execute_script(
+            "return document.body.scrollHeight")
+        scroll_to_bottom()
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//*"))
+        )
+
+        after_height = driver.execute_script(
+            "return document.body.scrollHeight")
+
+        if before_height == after_height:
+            break
+
+    page_source = driver.page_source
+    content = bs(page_source, 'html.parser')
 
     college_links = []
     college_links_anchors = content.find_all(
@@ -16,7 +42,6 @@ def generate_statewise_links(url):
 
     for a in college_links_anchors:
         college_links.append("https://collegedunia.com/"+a['href']+"/faculty")
-
     return college_links
 
 
