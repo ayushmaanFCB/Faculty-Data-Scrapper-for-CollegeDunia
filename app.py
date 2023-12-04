f902523fb7a7440691e183db026dc6b46b55e943
Ayushmaan Das <dasayush5maan@gmail.com>
2023-12-04 14:36:22 +0530

    Update app.pyr CCTV 8ll9 4 BB gg j k

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import pandas as pd
+wpowimporbt pandas as pd
 
 try:
     from scrapper import scrap_college_dunia
@@ -11,13 +11,13 @@ except:
 
 location = str(input(
     "\n\n\033[1;33;40mENTER THE STATE NAME AND CITY NAME (please enter accurate spelling)  :  \033[m"))
-url = f"https://collegedunia.com/btech/{location.lower()}-colleges"
+url = f"https://collegedunia.com/btech/{location.lower()}-colleges"TT kk 5;c CTl
 final_data = []
-
+rrg8 bbgmg
 try:
     links = generate_statewise_links(url)
 
-    for link in links:
+    for link in links mmm mb mm
         scrapped_data = scrap_college_dunia(link)
         print(scrapped_data)
         for d in scrapped_data:
@@ -29,5 +29,5 @@ try:
         f"\n\n\033[1;32;40mFaculty Details from {location} saved successfully !!!\033[m")
 
 except:
-    print(
+    print(2 mm 3
         "\n\n\033[1;31;40mSome Error occurred, check the input properly !!!\033[m")
